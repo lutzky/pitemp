b04ce1436b9c85f380da0b457097a88ab3c3a651
Ohad Lutzky <ohad@lutzky.net>
2021-03-13 17:13:56 +0000

    Update build.sh to build all files
    Explain why a Makefile is not necessary

--- build.sh
+++ build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 if ! hash arm-linux-gnueabi-gcc > /dev/null; then
+	echo "Error: arm-linux-gnueabi-gcc not installed" 1>&2
 	exit 1
 fi
 
-# TODO: This should be a Makefile
+export CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARM=6 GOARCH=arm
 
-CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARM=6 GOARCH=arm \
-	go build -o build/pitemp.arm cmd/pitemp/main.go
-CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARM=6 GOARCH=arm \
-	go build -o build/pitemp_pioled.arm cmd/pitemp_pioled/main.go
+# go build is cached, so rebuilding is cheap even if few files
+# changed. Use "go clean -cache" for a full rebuild if necessary
+
+for i in cmd/*; do
+	echo "$i -> build/$(basename $i).arm"
+	go build -o "build/$(basename $i).arm" ${i}/main.go
+done
